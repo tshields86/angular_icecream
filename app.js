63770b17c274bdc9a4b10061b0f19294bb06459c
Travis Shields <travis.shields@gmail.com>
2016-05-29 19:21:49 -0400

    writes directives for product titles and panels

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 (function(){
-  var app = angular.module('store', [ ]);
+  var app = angular.module('store', ['store-products']);
   app.controller('StoreController', function(){
     this.products = iceCreams;
-  });
-  app.controller("PanelController", function(){
-    this.tab = 1;
-
-    this.selectTab = function(setTab){
-      this.tab = setTab;
-    };
-    this.isSelected = function(checkTab){
-      return this.tab === checkTab;
-    }
   });
   app.controller("ReviewController", function(){
     this.review = {};
